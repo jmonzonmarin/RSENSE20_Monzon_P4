bbfd6d8ff5e60e3dd917fa5c26ef5596bd07810c
JorgeMonzon <jmonzon96@gmail.com>
2020-11-05 19:19:54 +0100

    Apartado 2. Completado

---
+++ Pr4/Pr4.ino
@@ -0,0 +1,51 @@
+
+#include <WiFi.h>
+#include "time.h"
+
+const char* ssidMovil       = "AndroidAP_4359";
+const char* passwordMovil   = "RSENSE-2020";
+
+const char* ssidPC       = "AndroidAP_4359";
+const char* passwordPC   = "01234567";
+
+const char* ntpServer = "europe.pool.ntp.org";
+const long  gmtOffset_sec = 3600;
+const int   daylightOffset_sec = 3600;
+
+void printLocalTime()
+{
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+    return;
+  }
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  
+  //connect to WiFi
+  Serial.printf("Connecting to %s ", ssidMovil);
+  WiFi.begin(ssidMovil, passwordMovil);
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+  }
+  Serial.println(" CONNECTED");
+  
+  //init and get the time
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  printLocalTime();
+
+  //disconnect WiFi as it's no longer needed
+  WiFi.disconnect(true);
+  WiFi.mode(WIFI_OFF);
+}
+
+void loop()
+{
+  delay(1000);
+  printLocalTime();
+}
